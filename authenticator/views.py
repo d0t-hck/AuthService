c3915d6adde5fdbd1c166e26f2103689d3600d98
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-01-25 14:31:23 +0600

    refactoring
    refactoring of Views code, added new functions

--- authenticator/views.py
+++ authenticator/views.py
@@ -2,142 +2,169 @@ from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework.serializers import Serializer
 
-from authenticator.serializers import UserSerializer
+from authenticator.serializers import RoleSerializer, UserSerializer
 from .models.user import User
 from .models.role import Role
 from .models.token import Token
 from django.core import serializers
-from django.forms.models import model_to_dict
 from rest_framework.parsers import JSONParser
 from . import hasher
 import json
-
-# Create your views here.
 
 
 def refresh_token(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         payload = hasher.decode_refresh_token(data['refresh_token'])
-        if (payload is None):
-            return HttpResponse(status=401)
-        token = __make_token(payload['user'])
+        if (payload is None or payload['user'] != data['email']):
+            return JsonResponse({}, status=401)
+        try:
+            user = User.objects.get(email=payload['user'])
+        except User.DoesNotExist:
+            return JsonResponse({}, status=401)
+        token = __make_token(user, data['refresh_token'])
+        if token is None:
+            return JsonResponse({}, status=401)
         return JsonResponse({"access_token": token.access_token, "refresh_token": token.refresh_token})
 
 
-def get_roles(request):
+def roles(request):
     if request.method == 'GET':
         roles = serializers.serialize('json', Role.objects.all())
-        return HttpResponse(roles, content_type='application/json')
-
-
-def get_role(request, id):
-    if request.method == 'GET':
-        role = Role.objects.get(id=id)
-        return JsonResponse(model_to_dict(role))
-
-
-def add_role(request):
-    data = json.loads(request.body.decode('utf-8'))
-    role = Role(name=data['role'], description=data['desc'])
-    role.save()
-    return JsonResponse(model_to_dict(role))
-
-
-def sign_up(request):
-    if request.method == 'POST':
+        return JsonResponse(RoleSerializer(roles).data, safe=False)
+    elif request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
-        encoded = hasher.hash_password(data['password'])
-        user = User(email=data['email'], first_name=data['first_name'],
-                    last_name=data['last_name'], password=encoded, role=Role.objects.get(id=1))
-        user.save()
-    return JsonResponse(model_to_dict(user))
+        role = Role(name=data['role'], description=data['desc'])
+        role.save()
+        return JsonResponse(RoleSerializer(role).data, safe=False)
 
 
-def sign_in(request):
+def role_detail(request, id):
+    try:
+        role = Role.objects.get(id=id)
+    except Role.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == 'GET':
+        return JsonResponse(RoleSerializer(role).data)
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        if (not data):
+            return JsonResponse({}, status=204)
+        if ('name' in data and len(data['name']) != 0):
+            role.name = data['name']
+        if ('description' in data and len(data['description']) != 0):
+            role.description = data['description']
+        role.save()
+        return JsonResponse(RoleSerializer(role).data)
+    elif request.method == 'DELETE':
+        role.delete()
+        return JsonResponse({}, status=204)
+
+
+def authorize(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         try:
             user = User.objects.get(email=data['email'])
         except User.DoesNotExist:
-            return HttpResponse(status=404)
-        new = hasher.check_password(data['password'], user.password)
+            return JsonResponse({}, status=404)
         if hasher.check_password(data['password'], user.password):
-            # access_token = hasher.encode_access_token(user.email)
-            # refresh_token = hasher.encode_refresh_token(user.email)
-            # try:
-            #     token = Token.objects.get(user=user.id)
-            #     token.access_token = access_token
-            #     token.refresh_token = refresh_token
-            # except Token.DoesNotExist:
-            #     token = Token.objects.create(user = user, access_token = access_token, refresh_token = refresh_token)
-            # token.save()
             token = __make_token(user)
             return JsonResponse({"access_token": token.access_token, "refresh_token": token.refresh_token})
         else:
-            return HttpResponse(status=401)
+            return JsonResponse({}, status=401)
 
 
-def get_users(request):
-    users = serializers.serialize('json', User.objects.all())
-    return HttpResponse(users, content_type='application/json')
+def logout(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        if not __logout(data['email']):
+            return JsonResponse({}, status=404)
+        return JsonResponse()
 
 
-def get_users(request, id):
-    user = User.objects.get(id=id)
-    return JsonResponse(model_to_dict(user))
-
-
-def get_users(request, email):
-    user = User.objects.get(email=email)
-    return JsonResponse(model_to_dict(user))
-
-
-def user_list(request):
+def users(request):
     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+        data = json.loads(request.body.decode('utf-8'))
+        encoded = hasher.hash_password(data['password'])
+        user = User(email=data['email'], first_name=data['first_name'],
+                    last_name=data['last_name'], password=encoded, role=Role.objects.get(id=1))
+        user.save()
+        return JsonResponse(UserSerializer(user).data)
 
 
-def user_detail(request, id):
+def user_detail(request, email):
     try:
-        user = User.objects.get(id=id)
+        user = User.objects.get(email=email)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
+        return JsonResponse({}, status=404)
 
     if request.method == 'GET':
         serializer = UserSerializer(user)
         return JsonResponse(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = UserSerializer(user, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+        data = json.loads(request.body.decode('utf-8'))
+        if not data:
+            return JsonResponse({}, status=204)
+        user = __update_user(user, data)
+        return JsonResponse(UserSerializer(user).data)
 
     elif request.method == 'DELETE':
         user.delete()
-        return HttpResponse(status=204)
+        return JsonResponse({}, status=204)
 
 
-def __make_token(user):
+def __make_token(user, old_token=None):
     access_token = hasher.encode_access_token(user.email)
     refresh_token = hasher.encode_refresh_token(user.email)
     try:
         token = Token.objects.get(user=user.id)
+        if (old_token != None and token.refresh_token != old_token):
+            return None
         token.access_token = access_token
         token.refresh_token = refresh_token
     except Token.DoesNotExist:
-        token = Token.objects.create(user=user, access_token = access_token, refresh_token = refresh_token)
+        token = Token.objects.create(
+            user=user, access_token=access_token, refresh_token=refresh_token)
     token.save()
     return token
+
+
+def __update_user(user, data):
+    if ('email' in data and len(data['email'])!=0):
+        __logout(user.email)
+        user.email = data['email']
+    if ('first_name' in data and len(data['first_name'])!=0):
+        user.first_name = data['first_name']
+    if ('last_name' in data and len(data['last_name'])!=0):
+        user.last_name = data['last_name']
+    if ('role' in data and len(data['role'])!=0):
+        try:
+            new_role = Role.objects.get(id=data['role'])
+            user.role = new_role
+        except Role.DoesNotExist:
+            pass
+    if ('password' in data and len(data['password'])!=0):
+        if (hasher.check_password(data['password']['old'], user.password)):
+            if (data['password']['new'] == data['password']['confirm']):
+                user.password = hasher.hash_password(
+                    data['password']['new'])
+                __logout(user.email)
+    user.save()
+    return user
+
+
+def __logout(email):
+    try:
+        # Not the efficent way, but works
+        user_id = User.objects.only('id').get(email=email)
+        token = Token.objects.get(user=user_id)
+        token.delete()
+    except (User.DoesNotExist, Token.DoesNotExist):
+        return False
+    return True
