fe733e12b2ff9f3c265a9ae135bd905952970798
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-01-25 14:21:23 +0600

    refactor serializer

--- authenticator/serializers.py
+++ authenticator/serializers.py
@@ -1,30 +1,21 @@
+from pyexpat import model
 from django.db.models.base import Model
 from rest_framework import fields, serializers
 from .models import User, Role, Code, Token
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'role']
-
-class PostUserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    email = serializers.EmailField(required=True)
-    first_name = serializers.CharField(required=True, max_length=50)
-    last_name = serializers.CharField(required=True, max_length=50)
-    role = serializers.IntegerField(default=1)
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = ['id', 'name', 'description']
 
-class TokenSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Token
-        fields = ['url', 'user', 'acces_token', 'refresh_token']
 
-class CodeSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Code
-        fields = ['url', 'code', 'user', 'is_used']
+        model = User
+        exclude = ['password']
+    id = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField(required=True)
+    first_name = serializers.CharField(required=True, max_length=50)
+    last_name = serializers.CharField(required=True, max_length=50)
+    role = RoleSerializer()
