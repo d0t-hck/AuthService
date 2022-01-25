ad985fad2358332fea67fe4d6197e4dfb6b92df7
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-01-25 14:28:50 +0600

    update token model
    changed relation between user to One-To-One

--- authenticator/models/token.py
+++ authenticator/models/token.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from .user import User
 
+
 class Token(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE)
+    user = models.OneToOneField(User, on_delete=CASCADE, primary_key=True)
     access_token = models.CharField(max_length=255)
     refresh_token = models.CharField(max_length=255)
