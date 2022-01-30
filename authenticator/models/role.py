2996983128e290d88cb0a7e11c5eef55683d7bfa
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-01-31 02:32:03 +0600

    added default ordering

--- authenticator/models/role.py
+++ authenticator/models/role.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 class Role(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['name']
--- authenticator/models/user.py
+++ authenticator/models/user.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from .role import Role
 
+
 class User(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
@@ -12,3 +13,6 @@ class User(models.Model):
     last_login = models.DateTimeField
     password_update = models.DateField
     role = models.ForeignKey(Role, on_delete=CASCADE)
+
+    class Meta:
+        ordering = ['email']
